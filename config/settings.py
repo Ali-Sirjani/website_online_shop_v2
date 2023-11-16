8f9f4b24827a08789d76303c9290fbc279716649
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-16 23:58:06 +0330

    feat: Configure and Implement Media Handling
    This commit implements media handling in the project. Configuration settings have been added to specify the media URL and root. Additionally, the necessary URL patterns have been included to serve media files during development. This setup ensures proper management and delivery of media content within the application.

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')), )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
