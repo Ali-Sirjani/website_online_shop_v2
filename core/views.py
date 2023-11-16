91b92ce662c350ae04a0497e436420adce8088e5
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-17 00:43:44 +0330

    feat: Add ProfileView
    This commit introduces the `ProfileView` class, a generic `UpdateView` responsible for displaying and updating user profile information. The view requires user authentication, utilizing the `LoginRequiredMixin` for access control. It uses the `Profile` model and `ProfileForm` for rendering and handling profile data. The associated template is 'core/profile.html', and upon successful update, the view redirects to the 'core:profile' URL.
    
    feat: Add URLs for Core App
    
    This commit adds a URL configuration for the 'core' app. It includes a path for the 'profile/' endpoint, mapped to the `ProfileView` class-based view. The namespace 'core' is utilized to organize and differentiate URLs within the project.
    
    feat: Include Core App URLs in Project Configuration
    
    This commit includes the URLs of the 'core' app within the project's configuration. The 'accounts/' endpoint now maps to the URLs defined in the 'core.urls' module, allowing access to features provided by the 'core' app.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Profile
+from .forms import ProfileForm
+
+
+class ProfileView(LoginRequiredMixin, generic.UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'core/profile.html'
+    success_url = reverse_lazy('core:profile')
+    context_object_name = 'profile'
+
+    def get_object(self, queryset=None):
+        profile_user, create = Profile.objects.get_or_create(user=self.request.user)
+        return profile_user
