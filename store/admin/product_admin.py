67df9aed79062bb981bd933b3d12ea8654f6dccc
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-17 21:56:14 +0330

    feat: Add CategoryAdmin in store app
    This commit introduces an enhanced `CategoryAdmin` class in the store app. The admin now displays the product count for each category, providing a quick overview of associated products. Users can conveniently navigate to the product changelist by clicking on the count. This enhancement improves the user experience in the Django admin for category management.
    
    feat: Add enhanced ProductAdmin in store app
    
    This commit introduces an enhanced `ProductAdmin` class in the store app. The admin now features a structured layout with categorized fieldsets for better organization and readability. The `datetime_created` and `datetime_updated` fields are marked as readonly to prevent unintended modifications. The list display includes essential information such as title, price, creation and update timestamps, and the product's activation status. The admin is further improved with search and autocomplete capabilities for efficient product management.

---
+++ store/admin/product_admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.db.models import Count
+from django.shortcuts import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+
+from mptt.admin import MPTTModelAdmin
+from jalali_date.admin import ModelAdminJalaliMixin
+
+from ..models import Category, Product
+from ..forms import ProductFormAdmin
+
+
+@admin.register(Category)
+class CategoryAdmin(MPTTModelAdmin):
+    list_display = ('name', 'products_count')
+    search_fields = ('name',)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(produc_count=Count('products'))
+
+    def products_count(self, obj):
+        url = (reverse('admin:store_product_changelist') + '?' + urlencode({'category': obj.pk}))
+        return format_html('<a href="{}">{}</a>', url, obj.produc_count)
+
+
+@admin.register(Product)
+class ProductAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    form = ProductFormAdmin
+    fieldsets = (
+        (_('Basic Information'), {
+            'fields': ('title', 'description', 'short_description', 'category')
+        }),
+        (_('Pricing'), {
+            'fields': ('price', 'discount', 'discount_price', 'discount_timer')
+        }),
+        (_('Inventory and Slug'), {
+            'fields': ('inventory', 'slug_change', 'slug')
+        }),
+        (_('Status and Timestamps'), {
+            'fields': ('is_active', 'datetime_created', 'datetime_updated')
+        }),
+    )
+    readonly_fields = ('datetime_created', 'datetime_updated',)
+    list_display = ('title', 'price', 'datetime_created', 'datetime_updated', 'is_active')
+    ordering = ('-datetime_updated',)
+    search_fields = ('title',)
+    autocomplete_fields = ('category',)
