bc8236dfd30142cb3ef0d391bb6b3f2712d2b2ae
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-20 17:44:41 +0330

    feat: Add Date Range Filter to ProductFilter
    This commit introduces two utility functions, `datetime_to_dict_func` and `validate_datetime_updated`, and adds a `datetime_updated` filter to the `ProductFilter` in the `product_filters.py` module.
    
    1. datetime_to_dict_func: A utility function to convert a datetime object to a dictionary of its components.
    
    2. validate_datetime_updated: A utility function to validate and convert start and stop datetime values from the Solar calendar to the Gregorian calendar. The function returns a slice of datetime values in the Gregorian calendar.
    
    3. ProductFilter: The `datetime_updated` filter is added to the filter set, allowing users to filter products based on the date range of their last update. The filter utilizes the introduced utility functions for date conversion and validation and is implemented through the `custom_filter_datetime` method.
    
    These enhancements improve the product search functionality by enabling users to filter products based on their last update date.

--- store/filters/product_filters.py
+++ store/filters/product_filters.py
@@ -1,15 +1,48 @@
 from django.db.models import F, Case, When, IntegerField
+from django.utils.translation import gettext_lazy as _
 import django_filters
+from django_filters import widgets
+from persiantools.jdatetime import JalaliDateTime
 
 from ..models import Product
 
 
+def datetime_to_dict_func(dt_obj):
+    return {
+        'year': dt_obj.year,
+        'month': dt_obj.month,
+        'day': dt_obj.day,
+        'hour': dt_obj.hour,
+        'minute': dt_obj.minute,
+        'second': dt_obj.second,
+        'microsecond': dt_obj.microsecond
+    }
+
+
+def validate_datetime_updated(value):
+    if value.start:
+        datetime_dict_start = datetime_to_dict_func(value.start)
+        value_start = JalaliDateTime(**datetime_dict_start).to_gregorian()
+    else:
+        value_start = None
+
+    if value.stop:
+        datetime_dict_stop = datetime_to_dict_func(value.stop)
+        value_stop = JalaliDateTime(**datetime_dict_stop).to_gregorian()
+    else:
+        value_stop = None
+
+    return slice(value_start, value_stop, None)
+
+
 class ProductFilter(django_filters.FilterSet):
     price = django_filters.NumericRangeFilter(method='custom_filter_price')
+    datetime_updated = django_filters.DateFromToRangeFilter(label=_('date'), widget=widgets.DateRangeWidget(
+        attrs={'class': 'jalali_date-date'}), method='custom_filter_datetime')
 
     class Meta:
         model = Product
-        fields = ('price', 'discount',)
+        fields = ('price', 'datetime_updated', 'discount')
 
     def custom_filter_price(self, queryset, name, value):
         start_value, stop_value = value.start, value.stop
@@ -31,3 +64,19 @@ class ProductFilter(django_filters.FilterSet):
             queryset_based_price = queryset_based_price.filter(effective_price__lte=stop_value)
 
         return queryset_based_price
+
+    def custom_filter_datetime(self, queryset, name, value):
+        value_validate = validate_datetime_updated(value)
+
+        start_value, stop_value = value_validate.start, value_validate.stop
+
+        if start_value and stop_value:
+            queryset = queryset.filter(datetime_updated__gte=start_value, datetime_updated__lte=stop_value)
+
+        elif start_value:
+            queryset = queryset.filter(datetime_updated__gte=start_value)
+
+        elif stop_value:
+            queryset = queryset.filter(datetime_updated__lte=stop_value)
+
+        return queryset
