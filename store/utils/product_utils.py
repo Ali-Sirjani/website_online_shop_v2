6c254cafabfc2d361f8e8f3d11f834961e55fa67
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-21 19:30:26 +0330

    feat: Add 'optimize_product_query' utility function
    This commit introduces a utility function named 'optimize_product_query' in the store's utils. The function optimizes a given product queryset by using 'prefetch_related' with 'Prefetch' objects to efficiently fetch related data, including specifications, color and size values, and images. This aims to reduce the number of database queries when retrieving product information.

--- store/utils/product_utils.py
+++ store/utils/product_utils.py
@@ -1,4 +1,6 @@
-from django.db.models import F, Case, When, IntegerField
+from django.db.models import F, Case, When, IntegerField, Prefetch
+
+from ..models import ProductColorAndSizeValue, ProductSpecificationValue
 
 
 def sort_product_queryset(sort_num, queryset):
@@ -29,5 +31,27 @@ def sort_product_queryset(sort_num, queryset):
 
         else:
             queryset = queryset.order_by(sort_by)
+
+    return queryset
+
+
+def optimize_product_query(queryset):
+    """
+    Prefetching related data to optimize database queries
+    - 'specs_values': Prefetching 'ProductSpecificationValue' objects with their related 'specification' objects
+    - 'color_size_values': Prefetching 'ProductColorAndSizeValue' objects with their related 'color' and 'size' objects
+    - 'images': Prefetching related 'Image' objects
+    """
+    queryset = queryset.prefetch_related(
+        Prefetch(
+            'specs_values',
+            queryset=ProductSpecificationValue.objects.select_related('specification')
+        ),
+        Prefetch(
+            'color_size_values',
+            queryset=ProductColorAndSizeValue.objects.select_related('color', 'size')
+        ),
+        'images',
+    )
 
     return queryset
