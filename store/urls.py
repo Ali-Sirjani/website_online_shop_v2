1a8a19530a64b6272adb83b9a5bc9ca1a882b1d7
Ali Sirjani <alisirjani1234@gmail.com>
2023-11-21 20:11:56 +0330

    refactor: Update and add URLs in the 'store' app
    This commit updates the existing 'product_list' URL to improve clarity and adds new URLs for the 'home' page and the user's favorite product list. The 'HomePageView' and 'ProductUserLikedView' are appropriately associated with the new URLs.

--- store/urls.py
+++ store/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.ProductsListView.as_view(), name='product_list'),
+    path('home', views.HomePageView.as_view(), name='home_page'),
+    path('list/', views.ProductsListView.as_view(), name='product_list'),
     path('search/', views.ProductSearchView.as_view(), name='product_search'),
     path('favorite/', views.favorite_view, name='set_favorite_product'),
+    path('favorite/list/', views.ProductUserLikedView.as_view(), name='favorite_product_list'),
     re_path(r'category/(?P<slug>[-\w]+)/', views.CategoryView.as_view(), name='category_page'),
     re_path(r'(?P<slug>[-\w]+)/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
