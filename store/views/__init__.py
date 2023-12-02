92dab1f08aa873d4c0cbbf306b60d25cf585bb6f
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-02 23:48:44 +0330

    feat: Introduce ZarinPal sandbox payment views for payment initiation and callback handling
    The 'sandbox_process_payment' view is implemented to facilitate the initiation of a test payment transaction through the ZarinPal sandbox portal. For authenticated users, it checks the user's shopping cart and redirects to the ZarinPal portal, preparing the necessary payment request data. For anonymous users, it retrieves the order information from the session and proceeds accordingly.
    
    The 'sandbox_callback_payment' view is designed to handle the callback from ZarinPal after the payment attempt. It checks the payment status and takes appropriate actions. In case of a successful payment, it updates the order status, marks it as completed, and processes the associated items. If the payment is unsuccessful, it utilizes the 'zarin_errors' function to provide detailed error messages to the user.
    
    These views enhance the development environment for testing ZarinPal payment integration, allowing for comprehensive simulation of payment transactions and robust handling of callback scenarios. This implementation provides a crucial component for testing and refining the payment flow within the application.

--- store/views/__init__.py
+++ store/views/__init__.py
@@ -1,3 +1,4 @@
 from .product_views import (ProductsListView, ProductSearchView, ProductDetailView, favorite_view,
                             ProductUserLikedView)
-from .order_views import (update_color_size_drop, update_item, cart_view, checkout_view)
+from .order_views import (update_color_size_drop, update_item, cart_view, checkout_view, sandbox_process_payment,
+                          sandbox_callback_payment)
