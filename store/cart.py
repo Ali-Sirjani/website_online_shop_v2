70196d86c8eecf891362aea799b68cd5a64420a2
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-02 18:02:22 +0330

    feat: Introduce 'set_price_item', 'update_quantity', and 'clear_cart' methods in 'Cart' class
    Introduced the 'set_price_item' method in the 'Cart' class to dynamically set price-related information for a specific item in the shopping cart. This method calculates and sets the base price, discount, and discounted price, considering additional costs such as size-specific prices.
    
    The 'update_quantity' method allows dynamic updates to the quantity of a product in the shopping cart based on specified actions. It handles scenarios such as addition, removal, and deletion of items, considering various conditions and notifying the user about the performed actions.
    
    The 'clear_cart' method is implemented to remove all items from the shopping cart, effectively resetting it to an empty state. After clearing the cart, the updated state is saved to reflect the changes in the user's session.
    
    These methods contribute to the functionality and versatility of the shopping cart, providing mechanisms for updating quantities, managing prices, and clearing the cart for a seamless user experience.

--- store/cart.py
+++ store/cart.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+from django.shortcuts import get_object_or_404
 
 from .models import Product, ProductColorAndSizeValue
 
@@ -73,4 +76,111 @@ class Cart:
 
                 del delete_key_list
 
+        self.save()
+
+    def set_price_item(self, item, product_pk, product_color_size_pk):
+        """
+        Set the price-related information for a specific item in the shopping cart.
+
+        Args:
+            item: The dictionary representing the item in the cart.
+            product_pk: The primary key of the associated product.
+            product_color_size_pk: The primary key of the color and size information,
+                                  or 'None' if there is no specific color and size.
+
+        This method calculates and sets the price-related information, including the
+        base price, discount, and discounted price for the given item. Additional costs,
+        if any (e.g., size-specific prices), are considered in the calculation.
+        """
+        product_obj = get_object_or_404(Product.active_objs, pk=product_pk)
+
+        additional_cost = 0
+        if product_color_size_pk != 'None':
+            color_size_obj = get_object_or_404(ProductColorAndSizeValue, pk=product_color_size_pk)
+            if color_size_obj.size_price:
+                additional_cost = color_size_obj.size_price
+
+        item['price'] = product_obj.price + additional_cost
+        item['discount'] = product_obj.discount
+        item['discount_price'] = product_obj.discount_price
+        if product_obj.discount_price:
+            item['discount_price'] = product_obj.discount_price + additional_cost
+
+    def update_quantity(self, product_pk, product_color_size, action, quantity=1):
+        """
+        Update the quantity of a product in the shopping cart.
+
+        Args:
+            product_pk: The primary key of the product to update.
+            product_color_size: The color and size information for the product, or None.
+            action: The action to perform ('add', 'remove', 'delete_item').
+            quantity: The quantity to add, remove, or set. Defaults to 1.
+
+        This method allows for dynamic updates to the quantity of a product in the
+        shopping cart based on the specified action. It considers various scenarios
+        such as addition, removal, and deletion of items. Messages are used to notify
+        the user about the performed action, and the cart state is saved.
+
+        Example of cart item structure:
+        {
+            product_pk_str: {
+                product_color_size_pk or 'None': {
+                    'quantity': 12,
+                    'price': product.price,
+                    'discount': product.discount,
+                    'discount_price': product.discount_price
+                }
+            }
+        }
+        """
+        product_pk_str = str(product_pk)
+
+        if product_color_size:
+            product_color_size_pk = str(product_color_size.pk)
+        else:
+            product_color_size_pk = str(product_color_size)
+
+        if product_pk_str not in self.cart:
+            if quantity < 0:
+                return
+            self.cart[product_pk_str] = {}
+
+        cart_product = self.cart[product_pk_str]
+
+        if product_color_size_pk not in cart_product:
+            cart_product[product_color_size_pk] = {'quantity': quantity}
+            self.set_price_item(cart_product[product_color_size_pk], product_pk_str, product_color_size_pk)
+
+        if cart_product[product_color_size_pk]['quantity'] * -1 > quantity or action == 'delete_item':
+            del cart_product[product_color_size_pk]
+            messages.success(self.request, _('Delete product'))
+            self.save()
+            return
+
+        elif action == 'add':
+            cart_product[product_color_size_pk]['quantity'] += quantity
+            messages.success(self.request, _('Add product'))
+
+        elif action == 'remove':
+            if quantity < 0:
+                quantity *= -1
+
+            cart_product[product_color_size_pk]['quantity'] -= quantity
+            messages.success(self.request, _('Remove product'))
+
+        if cart_product[product_color_size_pk]['quantity'] <= 0:
+            del cart_product[product_color_size_pk]
+            messages.success(self.request, _('Delete product'))
+
+        self.save()
+
+    def clear_cart(self):
+        """
+        Clear the entire shopping cart.
+
+        This method removes all items from the shopping cart, effectively resetting
+        it to an empty state. After clearing the cart, the updated state is saved
+        to reflect the changes in the user's session.
+        """
+        del self.session['cart']
         self.save()
