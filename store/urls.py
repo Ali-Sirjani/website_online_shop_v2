0184ef9113d6f6aa2baecff2c6fd87a82de2f621
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-02 18:48:49 +0330

    feat: Introduce 'update_item' view for cart manipulation
    Introduced the 'update_item' view in the store app, providing functionality for updating and managing items in the shopping cart. This view handles actions such as adding, removing, or deleting items from the cart based on the provided JSON data. The view supports both authenticated and anonymous users, updating the user's order if authenticated, or the session-based cart for anonymous users.
    
    The view parses JSON data from the request body, performs necessary validations, and updates the cart accordingly. Actions include adding, removing, or deleting items, as well as clearing the entire cart. Appropriate messages are generated to provide feedback to the user.
    
    This view serves as a crucial component in the shopping cart functionality, enabling dynamic interactions and real-time updates for a seamless user experience.
    
    feat: Add 'update-item' URL for the 'update_item' view in the store app
    
    Added the URL pattern 'cart/update-item/' to the 'urls.py' file in the store app, mapping it to the 'update_item' view.

--- store/urls.py
+++ store/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('search/', views.ProductSearchView.as_view(), name='product_search'),
     path('favorite/', views.favorite_view, name='set_favorite_product'),
     path('favorite/list/', views.ProductUserLikedView.as_view(), name='favorite_product_list'),
+    path('cart/update-item/', views.update_item, name='update_item'),
     path('admin-drop/color-size/', views.update_color_size_drop, name='color_size_drop'),
     re_path(r'list/category/(?P<slug>[-\w]+)/', views.ProductsListView.as_view(), name='category_page'),
     re_path(r'(?P<slug>[-\w]+)/', views.ProductDetailView.as_view(), name='product_detail'),
--- store/views/order_views.py
+++ store/views/order_views.py
@@ -1,10 +1,14 @@
-from django.utils.translation import gettext_lazy as _
-from django.db.models import Q
-from django.http.response import JsonResponse
-from django.contrib import messages
 import json
 
-from ..models import Product
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
+from django.contrib import messages
+
+from ..models import Order, OrderItem, Product, ProductColor, ProductSize, ProductColorAndSizeValue, Coupon
+from ..cart import Cart
 
 
 def update_color_size_drop(request):
@@ -34,3 +38,100 @@ def update_color_size_drop(request):
 
         messages.error(request, _('Please enter a product'))
         return JsonResponse('Invalid pk', safe=False)
+
+
+def update_item(request):
+    try:
+        data = json.loads(request.body)
+    except json.JSONDecodeError:
+        messages.warning(request, _('Oops! Something went wrong with your request. Please try again.'
+                                    ' If the issue persists, contact our support team for assistance.'))
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    quantity = data.get('quantity')
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        quantity = 0
+
+    if quantity != 0:
+        customer = request.user
+
+        product_id = data.get('productId')
+        color = data.get('colorId')
+        size = data.get('sizeId')
+        color_size_pk = data.get('colorSizeId')
+        action = data.get('action')
+
+        color_obj = size_obj = product = product_color_size = None
+
+        if action != 'delete_cart':
+            product = get_object_or_404(Product.active_objs, pk=product_id, )
+            if color:
+                color_obj = get_object_or_404(ProductColor, pk=color, )
+            if size:
+                size_obj = get_object_or_404(ProductSize, pk=size, )
+
+            try:
+                if color_size_pk:
+                    product_color_size = product.color_size_values.get(Q(inventory__gt=0) | Q(inventory=None),
+                                                                       pk=color_size_pk, is_active=True)
+                else:
+                    product_color_size = product.color_size_values.get(color=color_obj, size=size_obj, is_active=True)
+
+            except ProductColorAndSizeValue.DoesNotExist:
+                if product.active_color_size().exists():
+                    messages.error(request, _('Please select color or size for product!'))
+                    return JsonResponse('chose color or size!', safe=False)
+
+        if customer.is_authenticated:
+            order, order_created = Order.objects.get_or_create(customer=customer, completed=False)
+            if action == 'delete_cart':
+                order.items.all().delete()
+                messages.success(request, _('Cart deleted'))
+                return JsonResponse('Cart deleted', safe=False)
+
+            try:
+                item = OrderItem.objects.get(order=order, product=product, color_size=product_color_size)
+            except OrderItem.DoesNotExist:
+                if quantity < 0:
+                    messages.error(request, _('Please for adding product to cart enter a positive quantity!'))
+                    return JsonResponse('enter valid quantity', safe=False)
+                item = OrderItem.objects.create(order=order, product=product, color_size=product_color_size)
+
+            if item.quantity * -1 > quantity or action == 'delete_item':
+                item.delete()
+                messages.success(request, _('Delete product'))
+                return JsonResponse('Delete product', safe=False)
+
+            elif action == 'add' and quantity > 0:
+                item.quantity += quantity
+                messages.success(request, _('Add product'))
+
+            elif action == 'remove':
+                if quantity < 0:
+                    quantity *= -1
+                item.quantity -= quantity
+                if item.quantity <= 0:
+                    item.delete()
+                    messages.success(request, _('Delete product'))
+                    return JsonResponse('Delete product', safe=False)
+
+                else:
+                    messages.success(request, _('Remove product'))
+
+            item.save()
+
+        else:
+            cart = Cart(request)
+            if action == 'delete_cart':
+                cart.clear_cart()
+                messages.success(request, _('Cart deleted'))
+            else:
+                cart.update_quantity(product_id, product_color_size, action, quantity)
+
+        return JsonResponse('finish update view', safe=False)
+
+    else:
+        messages.warning(request, _('Please enter a correct number!'))
+
+    return JsonResponse('finish 0', safe=False)
