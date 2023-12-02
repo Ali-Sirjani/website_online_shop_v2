1b243d080b8d3b12c70e36c7d8107d75be858934
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-02 23:54:14 +0330

    feat: Add URLs for checkout, sandbox payment processes, and response templates
    Introduced URLs for the checkout process, sandbox payment processes, and response templates. The 'checkout' URL maps to the checkout_view, 'sandbox_process' and 'sandbox_callback' URLs map to sandbox_process_payment and sandbox_callback_payment views, respectively. Additionally, response templates for success, fail, and again scenarios have been added for the checkout process. These URLs and templates are part of the payment processing flow and provide endpoints for handling various stages of the payment process.

--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, re_path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -11,6 +12,12 @@ urlpatterns = [
     path('favorite/list/', views.ProductUserLikedView.as_view(), name='favorite_product_list'),
     path('cart/', views.cart_view, name='cart_page'),
     path('cart/update-item/', views.update_item, name='update_item'),
+    path('checkout/', views.checkout_view, name='checkout'),
+    path('process/checkout/', views.sandbox_process_payment, name='sandbox_process'),
+    path('process/callback/', views.sandbox_callback_payment, name='sandbox_callback'),
+    path('checkout/success/', TemplateView.as_view(template_name='store/order/success.html')),
+    path('checkout/fail/', TemplateView.as_view(template_name='store/order/fail.html')),
+    path('checkout/again/', TemplateView.as_view(template_name='store/order/again.html')),
     path('admin-drop/color-size/', views.update_color_size_drop, name='color_size_drop'),
     re_path(r'list/category/(?P<slug>[-\w]+)/', views.ProductsListView.as_view(), name='category_page'),
     re_path(r'(?P<slug>[-\w]+)/', views.ProductDetailView.as_view(), name='product_detail'),
