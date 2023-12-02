d62d41a7d82ae34adc200dd05829d5ed888ce952
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-02 23:39:21 +0330

    feat: Introduce 'check_out_user_anonymous' function in 'order_utils'
    Added a new function, 'check_out_user_anonymous', to the 'order_utils' module. This function handles the checkout process for anonymous users, specifically those without an active session.
    
    The function performs the following key actions:
    
    1. Checks if the cart is empty and redirects to the product list if no items are present.
    2. Validates the order form and processes the user's email information.
    3. Creates or retrieves an order instance with the provided email, associating it with the cart's coupon if available.
    4. Updates the session with the order's primary key and maintains a list of order primary keys.
    5. Compares the calculated total with the user-entered total and proceeds with the checkout process if they match.
    6. Saves the order form, generates a tracking code if not already present, and updates the order with the form data.
    7. Deletes existing order items to accommodate changes during the anonymous user checkout.
    8. Creates new order items based on the contents of the cart.
    9. Validates and saves the shipping address form.
    10. Redirects the user to the sandbox process page upon successful completion.
    
    This function enhances the checkout experience for anonymous users, ensuring a smooth and secure transaction process.

--- store/utils/order_utils.py
+++ store/utils/order_utils.py
@@ -1,11 +1,11 @@
 import datetime
 
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
 from ..forms import OrderForm, ShippingAddressForm
-from ..models import Order, ShippingAddress
+from ..models import Order, OrderItem, ShippingAddress
 
 
 def check_out_user_login(request):
@@ -52,3 +52,75 @@ def check_out_user_login(request):
     items = order.act_items().order_by('-datetime_created')
 
     return form_order, form_shipping, order, items
+
+
+def check_out_user_anonymous(request, cart):
+    if cart.get_cart_items == 0:
+        messages.info(request, _('The Your cart is empty! Pleas first add some product in your cart.'))
+        return redirect('store:product_list')
+
+    form_order = OrderForm(request.POST or None)
+    form_shipping = ShippingAddressForm(request.POST or None)
+    if form_order.is_valid():
+
+        email_user = form_order.cleaned_data.get('email')
+
+        order, create = Order.objects.get_or_create(customer=None, email=email_user, completed=False,
+                                                    coupon=cart.coupon.get('coupon_pk'))
+
+        request.session['order_pk'] = order.pk
+
+        orders_obj_pk = request.session.get('orders_pk_list') or []
+
+        if not (order.pk in orders_obj_pk):
+            orders_obj_pk.append(order.pk)
+            request.session['orders_pk_list'] = orders_obj_pk
+
+        total = None
+        try:
+            total = int(form_order.cleaned_data.get('total'))
+        except ValueError:
+            messages.error(request, _('You change the data of checkout form!'))
+
+        final_order_total = None
+        if order.coupon and order.calculate_coupon_price(request):
+            final_order_total = order.get_cart_total_with_coupon
+
+        else:
+            final_order_total = order.get_cart_total
+
+        if total == final_order_total:
+            form_order = OrderForm(request.POST, instance=order)
+
+            if not order.tracking_code:
+                tracking_code = datetime.datetime.now().timestamp()
+                form_order.instance.tracking_code = tracking_code
+
+            form_order.save()
+
+            # delete order items for update in save_items_and_shipping_address_user_anonymous()
+            order.items.all().delete()
+
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    color_size=item.get('color_size'),
+                    quantity=item['quantity'],
+                    price=item['price'],
+                    discount=item['discount'],
+                    discount_price=item['discount_price'],
+                )
+
+            if form_shipping.is_valid():
+                shipping, create = ShippingAddress.objects.get_or_create(order=order)
+
+                form_shipping = ShippingAddressForm(request.POST, instance=shipping)
+                form_shipping.save()
+
+                return redirect('store:sandbox_process')
+
+        else:
+            messages.info(request, _('Something went wrong! Please try again.'))
+
+    return form_order, form_shipping
