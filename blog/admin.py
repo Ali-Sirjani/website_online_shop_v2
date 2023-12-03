3a91c4f9e743596145641384de60682c2c35cf85
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-03 23:05:15 +0330

    feat: Implement Admin Configuration for Tag Model
       This commit introduces the registration of the `Tag` model in the Django admin interface, accompanied by the `TagAdmin` class. The class defines how tags are presented and managed within the admin view.
    
       - List Display: Configures the fields displayed in the list view, including 'name,' 'slug,' 'datetime_created,' and 'datetime_updated.'
       - Search Fields: Enables searching for tags based on their name.
       - Ordering: Sets the default ordering of tags based on the 'datetime_updated' field.
    
    feat: Configure PostCommentInline for Post Model in Admin
    
       This commit introduces the `PostCommentInline` class, enhancing the admin interface for the 'Post' model. The inline class configures the display and management settings for related `PostComment` instances.
    
       - Model Association: Links the inline to the `PostComment` model.
       - Readonly Fields: Specifies 'datetime_updated' as a readonly field.
       - Displayed Fields: Determines the fields to be shown, including 'author,' 'text,' 'confirmation,' and 'datetime_updated.'
       - Ordering: Sets the ordering of comments based on 'datetime_updated' in descending order.
       - Extra Comment Fields: Allows adding multiple comments simultaneously.
       - Autocomplete Fields: Provides autocomplete functionality for the 'author' field.
       - Textarea Widget: Enhances the 'text' field presentation with a larger textarea widget for better readability.
    
    These commits enhance the Django admin interface for the 'blog' app, offering improved usability and functionality for managing tags and post comments.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db.models import TextField
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Tag, Post, PostComment
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'datetime_created', 'datetime_updated')
+    search_fields = ('name',)
+    ordering = ('datetime_updated',)
+
+
+class PostCommentInline(admin.TabularInline):
+    model = PostComment
+    readonly_fields = ('datetime_updated',)
+    fields = ('author', 'text', 'confirmation', 'datetime_updated')
+    ordering = ('-datetime_updated',)
+    extra = 1
+    autocomplete_fields = ('author',)
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'cols': 70, 'rows': 4})}
+    }
