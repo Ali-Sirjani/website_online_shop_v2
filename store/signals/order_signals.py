e149b95eff5bb7f875ad56ebfb7b38d9883c6c15
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-03 12:14:36 +0330

    feat: Merge session cart items into user's active order on login
    add_item_to_cart_from_session: This signal adds items to the user's cart upon login by retrieving the items stored in the session. If a coupon is present in the session, it is also applied to the order. The cart is then cleared, and a success message is displayed.
    
    feat: Enhance user login signal to associate orders with user information
    
    The 'set_user_for_order_with_customer_null' signal in 'order_signals.py' has been extended to include two distinct actions:
    
    1. Update Completed Orders: Identify completed orders with a null customer and matching email as the logged-in user. Set the user as the customer for these orders.
    
    2. Clear Uncompleted Orders: Locate uncompleted orders with a null customer and matching email as the logged-in user. Delete these orders along with their associated order items, ensuring a clean slate for managing the order.
    
    This enhancement ensures a seamless transition for users, associating their completed orders with the user account and clearing uncompleted orders for a more organized order management system upon login.

--- store/signals/order_signals.py
+++ store/signals/order_signals.py
@@ -1,7 +1,64 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.signals import user_logged_in
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
 
-from ..models import Product, ProductColorAndSizeValue, Order, OrderItem
+from ..models import Product, ProductColorAndSizeValue, Order, OrderItem, Coupon
+from ..cart import Cart
+
+
+@receiver(user_logged_in)
+def add_item_to_cart_from_session(sender, user, request, **kwargs):
+    if request.session.get('cart'):
+        coupon_dict = request.session.get('coupon')
+        coupon = None
+        if coupon_dict:
+            try:
+                coupon = Coupon.objects.get(pk=coupon_dict.get('coupon_pk'))
+
+            except Coupon.DoesNotExist:
+                pass
+
+        cart = Cart(request)
+        order, create = Order.objects.get_or_create(customer=user, completed=False)
+        order.coupon = coupon
+        order.save()
+        for item_session in cart:
+            product = item_session.get('product')
+
+            if product.is_active:
+                item, create = OrderItem.objects.get_or_create(
+                    product=item_session['product'],
+                    color_size=item_session.get('color_size'),
+                    order=order,
+                )
+
+                if create:
+                    item.quantity = item_session['quantity']
+                    item.save()
+
+                else:
+                    item.quantity += item_session['quantity']
+                    item.save()
+
+        messages.success(request, _('The product add to your cart'))
+        cart.clear_cart()
+
+
+@receiver(user_logged_in)
+def set_user_for_order_with_customer_null(sender, user, request, **kwargs):
+    orders_with_user_email_completed = Order.objects.filter(customer=None, email=user.email, completed=True)
+
+    if orders_with_user_email_completed.exists():
+        orders_with_user_email_completed.update(customer=user)
+
+    orders_with_user_email_uncompleted = Order.objects.filter(customer=None, email=user.email, completed=False)
+
+    if orders_with_user_email_uncompleted.exists():
+        order_items = OrderItem.objects.filter(order__in=orders_with_user_email_uncompleted)
+        order_items.delete()
+        orders_with_user_email_uncompleted.delete()
 
 
 @receiver(post_save, sender=Product)
