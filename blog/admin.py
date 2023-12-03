4c6638f7de62c59752e14ccf0ceb362c08b33435
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-03 23:09:13 +0330

    feat: Enhance Admin Configuration for Post Model
    This commit introduces improvements to the admin interface for the 'Post' model by registering the `PostAdmin` class. Key enhancements include:
    
    - Fieldsets: Organizes post information into distinct sections for better readability and organization. Sections cover 'Post Information,' 'Slug Settings,' and 'Date and Time.'
    
    - Readonly Fields: Specifies certain fields as readonly, preventing modification of 'author,' 'datetime_created,' and 'datetime_updated.'
    
    - Autocomplete Fields: Enhances user experience by providing autocomplete functionality for the 'tags' field.
    
    - List Display: Configures fields displayed in the list view, including 'title,' 'author,' 'can_published,' 'datetime_created,' and 'datetime_updated.'
    
    - Inlines: Integrates the `PostCommentInline` class to manage and display related post comments conveniently within the 'Post' admin view.
    
    - Search Fields: Enables searching for posts based on their 'title' and the associated author's 'username.'
    
    - List Filter: Enables filtering posts based on the 'can_published' status.
    
    - Ordering: Sets the default ordering of posts based on the 'datetime_updated' field.
    
    - Custom Save Model Method: Overrides the default `save_model` method to automatically set the author of a new post to the currently logged-in user.
    
    This commit improves the usability and functionality of the Django admin interface for the 'blog' app, providing administrators with enhanced tools for managing posts.

--- blog/admin.py
+++ blog/admin.py
@@ -23,3 +23,33 @@ class PostCommentInline(admin.TabularInline):
     formfield_overrides = {
         TextField: {'widget': Textarea(attrs={'cols': 70, 'rows': 4})}
     }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Post Information'), {
+            'fields': ('title', 'description', 'author', 'tags', 'can_published')
+        }),
+        (_('Slug Settings'), {
+            'classes': ('collapse',),
+            'fields': ('slug_change', 'slug')
+        }),
+        (_('Date and Time'), {
+            'classes': ('collapse',),
+            'fields': ('datetime_created', 'datetime_updated'),
+        }),
+    )
+    readonly_fields = ('author', 'datetime_created', 'datetime_updated')
+    autocomplete_fields = ('tags', )
+    list_display = ('title', 'author', 'can_published', 'datetime_created', 'datetime_updated')
+    inlines = (PostCommentInline, )
+    search_fields = ('title', 'author__username')
+    list_filter = ('can_published', )
+    ordering = ('datetime_updated',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+
+        super().save_model(request, obj, form, change)
