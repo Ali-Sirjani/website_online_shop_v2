2c07922e3e3c7c25739ac834767c8b2152c7aa70
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-03 22:47:40 +0330

    feat: Implement Post Model in Blog App
    Introduces the `Post` model within the 'blog' app, serving as a core component for managing blog posts. This model includes the following key features:
    
    - Tags: Utilizes a `ManyToManyField` relationship with the previously added `Tag` model, allowing posts to be associated with multiple tags.
    
    - Author: Establishes a foreign key relationship with the user model for the post's author, providing information about the creator.
    
    - Title and Description: Captures essential information such as the post's title and a rich text description using `RichTextField`.
    
    - Publication Status: Incorporates a boolean field (`can_published`) to indicate whether the post can be published.
    
    - Slug Management: Implements fields for managing the post's slug, with an option to automatically generate the slug based on the title.
    
    - Timestamp: Includes fields for tracking the creation and update datetime of the post.
    
    This commit lays the groundwork for creating and organizing blog content, offering flexibility in tag association and user attribution.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from ckeditor.fields import RichTextField
 
 
 class Tag(models.Model):
@@ -17,3 +19,25 @@ class Tag(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Post(models.Model):
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name=_('tags'))
+    author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, verbose_name=_('author'))
+
+    title = models.CharField(max_length=250, verbose_name=_('title'))
+    description = RichTextField(verbose_name=_('description'))
+    can_published = models.BooleanField(default=True, verbose_name=_('can published'))
+    slug_change = models.BooleanField(verbose_name=_('slug change'), help_text=_('If you want change the slug by name'))
+    slug = models.SlugField(unique=True, allow_unicode=True, blank=True, max_length=300, verbose_name=_('slug'),
+                            help_text=_('If field be empty it\'s automatic change by name '))
+
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime created'))
+    datetime_updated = models.DateTimeField(auto_now=True, verbose_name=_('datetime updated'))
+
+    class Meta:
+        verbose_name = _('post')
+        verbose_name_plural = _('posts')
+
+    def __str__(self):
+        return f'{self.title}'
