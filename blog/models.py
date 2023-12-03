24d0cc145ab75688a9daa2445c35d887d316f866
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-03 22:44:24 +0330

    feat: Introduce Tag Model for Blog App
    This commit adds the `Tag` model to the 'blog' app, facilitating the organization of content through tags. The model includes fields for the tag name, a customizable slug, and options for automatic slug generation based on the name. Timestamp metadata, including creation and update datetime, is also incorporated.
    
    The `Tag` model is a fundamental component for managing and categorizing blog content based on user-defined tags.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('name'))
+    slug_change = models.BooleanField(verbose_name=_('slug change'), help_text=_('If you want change the slug by name'))
+    slug = models.SlugField(unique=True, allow_unicode=True, blank=True, max_length=300, verbose_name=_('slug'),
+                            help_text=_('If field be empty it\'s automatic change by name '))
+
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_('datetime created'))
+    datetime_updated = models.DateTimeField(auto_now=True, verbose_name=_('datetime updated'))
+
+    class Meta:
+        verbose_name = _('tag')
+        verbose_name_plural = _('tags')
+
+    def __str__(self):
+        return f'{self.name}'
