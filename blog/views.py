d0fb67b346510f01612ac2d3a7bd90f05befd4f3
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-05 22:21:32 +0330

    feat: Implement PostSearchView
    This commit introduces the `PostSearchView` class within the 'blog' app, utilizing Django's generic `ListView` to handle post search functionality. Key features of the view include:
    
    - Model and Template Configuration: Specifies the 'Post' model as the source of data and the 'blog/search_page.html' template for rendering.
    
    - Context Object Name: Defines the context object name as 'posts' for passing search results data to the template.
    
    - Pagination: Implements pagination with a page size of 3 posts per page for search results.
    
    - Custom Queryset Handling: Utilizes the `get_queryset` method to process search queries. If a valid search query is present in the request, it filters posts based on title and tag name, with case-insensitive matching. The queryset is further optimized using `prefetch_related` to minimize database queries.
    
    - Context Data Handling: Implements the `get_context_data` method to pass the search query ('q') to the template for display.
    
    - Dispatch Method: Overrides the `dispatch` method to handle cases where the search query is empty or contains only whitespace. In such cases, it renders a specific template ('blog/search_q_none.html') to inform users.
    
    This commit enhances the 'blog' app by providing a dedicated view for searching and displaying posts, improving user experience and interaction.

--- blog/views.py
+++ blog/views.py
@@ -27,3 +27,40 @@ class PostListView(generic.ListView):
         optimize_queryset = queryset.prefetch_related('tags')
 
         return optimize_queryset
+
+
+class PostSearchView(generic.ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/search_page.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        queryset = []
+        request_get = self.request.GET
+
+        if 'q' in request_get:
+            form = PostSearchForm(request_get)
+            if form.is_valid():
+                q = form.cleaned_data['q']
+                queryset = Post.active_objs.filter(
+                    Q(title__icontains=q) | Q(tags__name__icontains=q)).distinct()
+
+                queryset = queryset.prefetch_related('tags')
+                self.q = q
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            context['q'] = self.q
+        except AttributeError:
+            context['q'] = None
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        q = request.GET.get('q')
+        if not q or q.isspace():
+            return render(self.request, 'blog/search_q_none.html')
+        return super().dispatch(request, *args, **kwargs)
