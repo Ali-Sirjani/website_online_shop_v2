6280c487fca77829b4ef9c36d276e0b6cfc60a76
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-05 22:18:28 +0330

    feat: Implement PostListView
    This commit introduces the `PostListView` class within the 'blog' app, utilizing Django's generic `ListView` for handling the display of posts. Key features of the view include:
    
    - Model and Template Configuration: Specifies the 'Post' model as the source of data and the 'blog/post_list.html' template for rendering.
    
    - Context Object Name: Defines the context object name as 'posts' for passing posts data to the template.
    
    - Pagination: Implements pagination with a page size of 2 posts per page.
    
    - Custom Queryset Handling: Utilizes the `get_queryset` method to dynamically filter posts based on the presence of a tag slug in the URL. If a tag slug is present, it filters posts by the specified tag; otherwise, it retrieves all active posts. The queryset is further optimized using `prefetch_related` to minimize database queries and improve performance.
    
    This commit enhances the 'blog' app by providing a versatile and optimized view for listing posts, accommodating both general post displays and tag-specific post filtering.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from .models import Post, PostComment
+from .forms import PostSearchForm, PostCommentForm
+
+
+class PostListView(generic.ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/post_list.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('slug')
+        if tag_slug:
+            queryset = Post.active_objs.filter(tags__slug=tag_slug)
+
+        else:
+            queryset = Post.active_objs.all()
+
+        optimize_queryset = queryset.prefetch_related('tags')
+
+        return optimize_queryset
