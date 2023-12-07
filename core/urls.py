b5633a9e4548c0adbef1c949b418f0a313edc4a7
Ali Sirjani <alisirjani1234@gmail.com>
2023-12-07 13:39:05 +0330

    feat: Add Contact Us Template
    This commit introduces the 'contact_us.html' template, which extends the '_base.html' template. The contact form template is designed to provide a clean and responsive layout for users to submit their inquiries. It includes a centered form with fields for the user's full name, email, phone number, and a message. The form is styled using the `crispy_forms` library for a better user experience.
    
    This addition enhances the user interface by providing a dedicated and visually appealing page for users to contact the website.
    
    feat: Add URL for ContactUsView
    
    This commit includes the addition of a URL pattern for the `ContactUsView` in the 'core/urls.py' file. The URL '/contact-us/' is now associated with the `ContactUsView` class-based view, providing users with access to the contact form page.

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ app_name = 'core'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home_page'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('contact-us/', views.ContactUsView.as_view(), name='contact_page'),
 ]
