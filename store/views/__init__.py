65dc5645a525cbd47be38b2bce51c0b6e5bbbc75
Ali Sirjani <alisirjani1234@gmail.com>
2024-01-08 21:59:34 +0330

    refactor: Enhance User Experience in Checkout View
    Streamlined and improved the checkout view for both authenticated and anonymous users. Displayed a helpful message for anonymous users, encouraging them to register for future order details. Implemented AJAX support to handle and display form errors dynamically, contributing to a more user-friendly checkout experience.
    
    feat: Add set_profile_info view
    
    Introduce a new view, set_profile_info, to handle AJAX requests for retrieving user profile information. This view, accessible only to authenticated users, is decorated with @require_GET and returns a JsonResponse containing essential user profile details.
    
    This commit enhances the application by providing a dedicated view for AJAX retrieval of user profile information, promoting modularity, improved user experience, and ensuring a GET request requirement.

--- store/views/__init__.py
+++ store/views/__init__.py
@@ -1,4 +1,4 @@
 from .product_views import (ProductsListView, ProductSearchView, ProductDetailView, favorite_view,
                             ProductUserLikedView, filter_size_based_color)
 from .order_views import (update_color_size_drop, update_item, cart_view, OrderDetailView, apply_coupon_view)
-from .payment_views import (checkout_view, sandbox_process_payment, sandbox_callback_payment)
+from .payment_views import (checkout_view, set_profile_info, sandbox_process_payment, sandbox_callback_payment)
